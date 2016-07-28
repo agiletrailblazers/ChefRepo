23fe2f489cd6352ae7bb1b616560aa74e7126c3f
krithi <kgnanasekaran@agiletrailblazers.com>
2016-07-28 12:41:51 -0400

    Change the validation client name

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,7 +2,7 @@ log_level                :info
 log_location             STDOUT
 node_name                'dev'
 client_key               '/home/jenkins/workspace/Chef-CookbookUpload/.chef/sasi.pem'
-validation_client_name   'sasi.pem'
+validation_client_name   'sasi'
 validation_key           '/home/jenkins/workspace/Chef-CookbookUpload/.chef/atb-chef-admin.pem'
 chef_server_url          'https://54.211.154.197/organizations/atb'
 syntax_check_cache_path  '/home/jenkins/workspace/Chef-CookbookUpload/.chef/syntax_check_cache'
