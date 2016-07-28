d2742d07bb935e4723bbc60fe517ccc5c9a29229
krithi <kgnanasekaran@agiletrailblazers.com>
2016-07-28 16:27:38 -0400

    Creating node_config

---
+++ node_config/node_bootstrap.sh
@@ -0,0 +1,2 @@
+cd ~/home/jenkins/workspace/Chef-CookbookUpload/.chef/
+sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-CookbookUpload/.chef/agiletrailblazers.pem -N Rigil_Node
