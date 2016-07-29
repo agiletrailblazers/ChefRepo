2f0b42e7777cec88687cf1d383fae5e459118fc4
Sasi Kumar <skumar@agiletrailblazers.com>
2016-07-28 20:43:00 -0400

    Added node_config to check for exisiting nodes

--- node_config/node_bootstrap.sh
+++ node_config/node_bootstrap.sh
@@ -1,6 +1,2 @@
 cd ~/home/jenkins/workspace/Chef-CookbookUpload/.chef/
-if [ "`knife node list`" == *Rigil_Node* ]; then
-  echo "Found a Node bootstrapped to the chef server!"
-else
-  sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-CookbookUpload/.chef/agiletrailblazers.pem -N Rigil_Node
-fi
+sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-CookbookUpload/.chef/agiletrailblazers.pem -N Rigil_Node
