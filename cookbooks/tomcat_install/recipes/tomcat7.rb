dabfdac905bc4e2fcd4fe144b151e7a4500cabf0
Sasi Kumar <skumar@agiletrailblazers.com>
2016-07-29 15:19:11 -0400

    edit tomcat7 recipe

--- cookbooks/tomcat_install/recipes/tomcat7.rb
+++ cookbooks/tomcat_install/recipes/tomcat7.rb
@@ -1,5 +1,4 @@
 #Install tomcat package
-execute "installing tomcat7" do
- command "sudo wget http://www.trieuvan.com/apache/tomcat/tomcat-7/v7.0.70/bin/apache-tomcat-7.0.70.tar.gz"
- command "sudo tar xzvf apache-7.0.70.tar.gz -C /etc/tomcat7/"
+package 'tomcat7' do
+ action :install
 end
