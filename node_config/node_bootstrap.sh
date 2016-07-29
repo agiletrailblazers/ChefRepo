c7ebb20e258f9b7abf80f4e9a5728d32729620dd
Sasi Kumar <skumar@agiletrailblazers.com>
2016-07-28 20:53:56 -0400

    edited node_bootstrap.sh to run the chef nodes run list from its server

--- node_config/node_bootstrap.sh
+++ node_config/node_bootstrap.sh
@@ -1,2 +1,10 @@
 cd ~/home/jenkins/workspace/Chef-CookbookUpload/.chef/
+
+# Bootstrap a node to its chef server
 sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-CookbookUpload/.chef/agiletrailblazers.pem -N Rigil_Node
+
+# ssh into the chef node and execute the chef client to run its run list from chef server
+ssh -i ./ssh/agiletrailblazers.com ubuntu@54.175.232.159;
+
+# Execute the chef node to run its run list from its Chef server
+sudo chef-client
