c7247d24e6debe973e51ce4e70da74dddae7f820
Sasi Kumar <skumar@agiletrailblazers.com>
2016-07-29 11:13:40 -0400

    Added role

--- node_config/node_bootstrap.sh
+++ node_config/node_bootstrap.sh
@@ -3,7 +3,7 @@
 cd ~/home/jenkins/workspace/Chef-CookbookUpload/.chef/
 
 # Bootstrap a node to its chef server
-sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-CookbookUpload/.chef/agiletrailblazers.pem -N Rigil_Node
+sudo knife bootstrap 54.175.232.159 --ssh-user ubuntu --sudo --identity-file /home/jenkins/workspace/Chef-CookbookUpload/.chef/agiletrailblazers.pem -N Rigil_Node -r 'role[tomcat]'
 
 # ssh into the chef node and execute the chef client to run its run list from chef server
 run-list(){
